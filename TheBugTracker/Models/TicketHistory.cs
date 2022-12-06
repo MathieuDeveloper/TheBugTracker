f3ae7be985960188bf6d3ce5596dcd5fe6322a4b
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-12-07 00:33:48 +0100

    Create TicketHistory.cs

---
+++ TheBugTracker/Models/TicketHistory.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheBugTracker.Models
+{
+    public class TicketHistory
+    {
+        //primary key
+        public int Id { get; set; }
+
+        //foreign key        
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        
+        [DisplayName("Updated Itemicket")]
+        public string Property { get; set; }
+
+        
+        [DisplayName("Previous")]
+        public string OldValue { get; set; }
+
+        
+        [DisplayName("Current")]
+        public string NewValue { get; set; }
+
+
+        [DisplayName("Date Modified")]
+        public DateTimeOffset Created { get; set; }
+
+
+        [DisplayName("Description of Change")]
+        public string Description { get; set; }
+
+
+        //foreign key 
+        // It is a string UserId is a BTUser défini comme string par microsoft
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        //Navigation properties
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser User { get; set; }
+
+    }
+}
