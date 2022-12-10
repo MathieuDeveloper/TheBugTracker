79fd9c5c2463873c75bd4ee54cfb4979d240b9d0
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-12-10 23:53:13 +0100

    2 models

---
+++ TheBugTracker/Models/Project.cs
@@ -0,0 +1,62 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBugTracker.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+
+        [DisplayName("Company")]
+        public int ConmpanyId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayName("Start")]
+        public DateTimeOffset StartDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("End")]
+        public DateTimeOffset EndDate { get; set; }
+
+        //Foreign key
+        [DisplayName("Project Priority")]
+        public int ProjectPriorityId { get; set; }
+
+        //image
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string FileContentType { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archived { get; set; }
+
+
+        //Navigation properties, pluriel ou singulier ?
+
+        public virtual Company Company { get; set; }
+        public virtual ProjectPriority ProjectPriority { get; set; }
+        public  virtual Member Members { get; set; }
+        public virtual Ticket Tickets { get; set; }
+
+    }
+}
---
+++ TheBugTracker/Models/ProjectPriority.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+        public int Id { get; set; }
+
+        //CF :  [DisplayName("Priority Name")]
+        [Display(Name = "Priority Name")]
+        public string Name { get; set; }
+    }
+}
