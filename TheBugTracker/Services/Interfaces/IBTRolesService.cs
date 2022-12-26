4ec3eee76de90120a6ded806824df55784c23423
MathieuDeveloper <topfermathieu@yahoo.fr>
2022-12-27 00:54:49 +0100

    Create IBTRolesService.cs

---
+++ TheBugTracker/Services/Interfaces/IBTRolesService.cs
@@ -0,0 +1,21 @@
+﻿using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTRolesService
+    {
+        public Task<bool> IsUserInRoleAsync(BTUser user, string roleName);
+
+        public Task<IEnumerable<string>> GetUserRolesAsync(BTUser user);
+
+        public Task<bool> AddUserToRoleAsync(BTUser user, string roleName);
+
+        //remove user from a role
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, string roleName);
+
+        //remove user from all roles
+        public Task<bool> RemoveUserFromRoleAsync(BTUser user, IEnumerable<string> roles);
+
+        public Task<List<BTUser>> GetUsersInRoleAsync(string roleName, int companyId);
+    }
+}
